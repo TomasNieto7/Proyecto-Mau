c46a2dca78fecac5f84cf534d2c72e40d0369c24
TomasNieto7 <tnieto07@alumnos.uaq.mx>
2024-06-10 20:53:04 -0600

    Add: added Route Notes

---
+++ routes/notes.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const notes = express.Router()
+const db = require('../database.js')
+
+notes.get('/', (req, res, next) => {
+    return res.status(200).json({
+        code: 1,
+        message: 'Bienvenido a Users'
+    })
+})
+
+notes.post('/newNote', async (req, res, next) => {
+    const {
+        owner,
+        title,
+        description,
+        type
+    } = req.body
+    const query = `INSERT INTO NOTES(owner, title, description, type) VALUES('${owner}', '${title}', '${description}', '${type}')`
+    if (owner && title && description && type) {
+        try {
+            const note = await db.query(query)
+            res.status(201).json({
+                code: 201,
+                message: "note registered!!"
+            })
+        } catch (error) {
+            console.log(error);
+            return res.status(404).json({
+                code: 404,
+                message: error
+            })
+        }
+    } else res.status(200).json({
+        code: 200,
+        message: "data missing"
+    })
+
+
+})
+
+notes.put('/edit', async (req, res, next) => {
+    const {
+        id,
+        owner,
+        title,
+        description,
+        type
+    } = req.body
+    const query = `UPDATE NOTES SET OWNER = '${owner}', TITLE = '${title}', DESCRIPTION = '${description}', TYPE = '${type}', WHERE ID = '${id}'`
+    if (id&&owner&&title&&description&&type) {
+        try {
+            const note = await db.query(query)
+            console.log(note);
+            note ? res.status(201).json({
+                code: 201,
+                message: note
+            }) : res.status(404).json({
+                code: 404,
+                message: "Something went wrong"
+            })
+
+        } catch (error) {
+            console.log(error);
+            return res.status(404).json({
+                code: 404,
+                message: error
+            })
+        }
+    } else res.status(404).json({
+        code: 404,
+        message: "data missing"
+    })
+
+})
+
+notes.delete('/delete', async (req, res, next) => {
+    const query = `DELETE FROM NOTES WHERE ID = '${req.body.id}'`
+    if (req.body.id) {
+        try {
+            const note = await db.query(query)
+            res.status(201).json({
+                code: 201,
+                message: 'note deleted'
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(404).json({
+                code: 404,
+                message: "Something went wrong"
+            })
+        }
+    } else res.status(404).json({
+        code: 404,
+        message: "data missing"
+    })
+})
+
+module.exports = notes
